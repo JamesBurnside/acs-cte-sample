bb53f8df3badfa9bacc0712f4cfd0147cd78b2e2
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-06-16 00:10:38 +0000

    Add a client id and scopes for application

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Providers } from '@microsoft/mgt-element';
+import { Msal2Provider } from '@microsoft/mgt-msal2-provider';
+
+Providers.globalProvider = new Msal2Provider({
+  clientId: 'a2ee26d1-a1e2-4289-b37b-1da484a72fb8',
+  scopes: ['User.Read', 'https://auth.msft.communication.azure.com/Teams.ManageCalls']
+});
 
 ReactDOM.render(
   <App />,
